1a29f76fa9ca4e11b96d3cba6e759163165995e3
Daniel Chadwick <djchadwick@outlook.com>
2023-02-01 21:09:34 +0000

    feat: Location (AvailabilityBlock: prototyping calendar design)

--- src/components/location/blocks/AvailabilityBlock.tsx
+++ src/components/location/blocks/AvailabilityBlock.tsx
@@ -1,30 +1,50 @@
 import { LocationBlock } from "./LocationBlock";
 
-const DayView = () => {
+const TimeView = ({ from, to }: { from: string; to: string }) => {
   return (
-    <div>
-      
+    <div className="w-20 bg-gradient-to-b from-[#2e026d] to-[#15162c] p-4 text-center">
+      <span className="flex flex-col text-white">
+        {from} - {to}
+      </span>
     </div>
   );
-}
+};
+
+// one component per file, however WIP calendar
+// convert date to Date, and format with date-fns?
+const DayView = ({ date }: { date: string }) => {
+  return (
+    <div className="flex flex-row">
+      <div className="align-center flex w-16 items-center justify-center">
+        <span className="text-md font-semibold">{date}</span>
+      </div>
+
+      <div className="flex flex-row gap-1">
+        <TimeView from="10:00" to="11:00" />
+        <TimeView from="11:00" to="12:00" />
+        <TimeView from="12:00" to="13:00" />
+      </div>
+    </div>
+  );
+};
+
+// one component per file, however WIP calendar
+const AvailabilityCalendar = () => {
+  return (
+    <>
+      <div className="flex flex-col gap-1">
+        <DayView date="31/01" />
+        <DayView date="01/01" />
+        <DayView date="02/01" />
+      </div>
+    </>
+  );
+};
 
 export const AvailabilityBlock = () => {
   return (
     <LocationBlock title="Availabilty">
-      <div className="flex flex-col md:flex-row">
-        <div className="lg:w-9/12"></div>
-
-        <div className="flex flex-col gap-1 lg:w-3/12">
-          {/* time picker */}
-          <div className="bg-gradient-to-b from-[#2e026d] to-[#15162c] p-4 text-center">
-            <span className="text-white">10:00 - 11:00</span>
-          </div>
-
-          <div className="bg-gradient-to-b from-[#2e026d] to-[#15162c] p-4 text-center">
-            <span className="text-white">11:00 - 12:00</span>
-          </div>
-        </div>
-      </div>
+      <AvailabilityCalendar />
     </LocationBlock>
   );
 };
